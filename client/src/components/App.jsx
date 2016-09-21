a7e51b5fab11d12bb08d19e0587211384b428cb9
Derek Hoang <hoangda23@gmail.com>
2016-09-20 17:16:20 -0700

    began chat app

---
+++ client/src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      word: 'hello'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
